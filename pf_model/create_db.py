6a029624da90288b20b39e32f6cf6c077cc78c50
mikgur <gurevich3004@gmail.com>
2018-12-19 22:39:22 +0300

    Move populate_db() to create_db.py and add minor changes in model.py
    As functions create_db and populate_db are basically perform
    preparation of a database for a first user, I decided to merge them.
    All preparations are now performed by create_db()

--- pf_model/create_db.py
+++ pf_model/create_db.py
@@ -1,19 +1,17 @@
 """Script for creating a new database for personal_finance_bot"""
 import logging
 
-from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import OperationalError, ProgrammingError
 from sqlalchemy_utils import database_exists, create_database
 
-from .model import base
+from .model import base, db, AccountType, CategoryType, Currency,\
+                       TransactionType
 import settings
 
 
 def create_db():
     try:
-        db_string = f"postgres://{settings.DB_USER}:{settings.DB_PASSWORD}\
-                    @{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_DATABASE}"
-        db = create_engine(db_string)
         if not database_exists(db.url):
             logging.info(f"Creating database {settings.DB_DATABASE}...")
             create_database(db.url)
@@ -22,5 +20,35 @@ def create_db():
         else:
             logging.warning(f"Database {settings.DB_DATABASE} exist. \
                              Please provide another DB_DATABASE in settings.py")
+
+        Session = sessionmaker(bind=db)
+        session = Session()
+
+        logging.info("Creating income/expense category types...")
+
+        income_cat = CategoryType(name='income')
+        expense_cat = CategoryType(name='expense')
+
+        logging.info("Creating 2 currencies...")
+
+        rub = Currency(name="Рубль", shortname="руб")
+        usd = Currency(name="Доллар", shortname="usd")
+
+        logging.info("Creating account type...")
+
+        ordinary_type = AccountType(name="general")
+
+        logging.info("Creating 2 transaction types - income and expense")
+
+        income_tr = TransactionType(name="income")
+        expense_tr = TransactionType(name="expense")
+
+        session.add_all([income_cat, expense_cat])
+        session.add_all([rub, usd, ordinary_type])
+        session.add_all([income_tr, expense_tr])
+
+        session.commit()
+
+        logging.info("Everything is ready. You can launch the bot!")
     except (OperationalError, ProgrammingError) as exc:
         logging.error(f"Error while working with DB: {exc}")
--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 import logging
 
 from pf_model.create_db import create_db
-from pf_model.populate_db import populate_db
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='pf_bot.log'
                     )
 create_db()
-populate_db()
