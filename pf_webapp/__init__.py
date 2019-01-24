a4e7eaff84267ac7a7ac042a6770cbbc6c8cea43
Mikhail Gurevich <gurevich3004@gmail.com>
2019-01-24 17:49:15 +0300

    Add user registration and login forms

--- pf_webapp/__init__.py
+++ pf_webapp/__init__.py
@@ -13,22 +13,6 @@ def create_app():
 
     @app.route("/", methods=["GET", "POST"])
     def index():
-        return render_template("user/login.html")
-        """if request.method == "POST":
-
-            if request.form["menu_button"] == "Categories":
-                user_id = 10179437
-                user_name = "Mikhail"
-                return render_template("category/index.html",
-                                       active_info="Categories",
-                                       user_name=user_name,
-                                       categories_list=data_observer.get_all_category_names(user_id)
-                                       )
-            elif request.form["menu_button"] == "Transactions":
-                return render_template("category/index.html", active_info="", categories_list=[])
-            else:
-                return render_template("category/index.html", active_info="", categories_list=[])
-        elif request.method == 'GET':
-            return render_template("category/index.html", active_info="", categories_list=[])"""
+        return render_template("category/index.html")
 
     return app
--- pf_webapp/user/views.py
+++ pf_webapp/user/views.py
@@ -1,9 +1,27 @@
 from flask import Blueprint, render_template
 
+from pf_webapp.user.forms import LoginForm, RegistrationForm
 
 blueprint = Blueprint("user", __name__, url_prefix="/user")
 
 
 @blueprint.route("/login")
 def login():
-    return render_template("user/login.html")
+    login_form = LoginForm()
+    return render_template("user/login.html", form=login_form)
+
+
+@blueprint.route("/process-login", methods=["POST"])
+def process_login():
+    return "Success"
+
+
+@blueprint.route("/register")
+def register():
+    registration_form = RegistrationForm()
+    return render_template("user/register.html", form=registration_form)
+
+
+@blueprint.route("/process-reg", methods=["POST"])
+def process_reg():
+    return "Success"
