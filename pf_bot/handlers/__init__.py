ee44d85e589e4279eedf429c3114485a577fb12d
Mikhail <gurevich3004@gmail.com>
2019-02-08 14:46:50 +0300

    Fix statistics plot bug
    Labels are now don't cropped out

--- pf_bot/handlers/__init__.py
+++ pf_bot/handlers/__init__.py
@@ -68,11 +68,13 @@ def show_accounts(bot, update):
 
 
 def show_statistics(bot, update):
-    user = update.message.from_user
+    user = update.effective_user
+    current_month = get_current_month()
+    last_month = get_last_month()
 
     reply_text = "Вот статистика расходов в текущем месяце:\n "
     for expense in data_observer.statistics_for_period_by_category(
-        user.id, get_current_month()['period']
+        user.id, current_month["period"]
     ):
         amount = f"{expense[0]:,.0f}".replace(",", " ")
         reply_text = "\n".join([reply_text, f"{expense[1]} - {amount}"])
@@ -81,7 +83,7 @@ def show_statistics(bot, update):
         [reply_text, f"Твои расходы в прошлом месяце:\n "]
     )
     for expense in data_observer.statistics_for_period_by_category(
-        user.id, get_last_month()['period']
+        user.id, last_month["period"]
     ):
         amount = f"{expense[0]:,.0f}".replace(",", " ")
         reply_text = "\n".join([reply_text, f"{expense[1]} - {amount}"])
@@ -90,25 +92,31 @@ def show_statistics(bot, update):
 
     #  Send the barplot with statistics
     current_data = data_observer.statistics_for_period_by_category(
-        user.id, get_current_month()['period']
+        user.id, current_month["period"]
     )
     df_current = pd.DataFrame(current_data, columns=['Amount', 'Category'])
-    df_current['Month'] = 'Current'
+    df_current['Month'] = current_month['name']
 
     previous_data = data_observer.statistics_for_period_by_category(
-        user.id, get_last_month()['period']
+        user.id, last_month["period"]
     )
     df_previous = pd.DataFrame(previous_data, columns=['Amount', 'Category'])
-    df_previous['Month'] = 'Previous'
+    df_previous['Month'] = last_month["name"]
 
     data = pd.concat([df_current, df_previous])
+    number_of_categories = len(data['Category'].unique())
     sns.set_style("darkgrid")
     sns.set_context("notebook", font_scale=1.5)
     sns.set_palette("deep")
     plot = sns.barplot(x="Category", y="Amount", hue="Month", data=data)
     plot.set_xlabel("")
+    plot.set_title("Expenses")
+    for item in plot.get_xticklabels():
+        item.set_rotation(-90)
     imgdata = BytesIO()
-    plot.figure.savefig(imgdata, format="png")
+    plot.figure.set_size_inches(10, 2.5*number_of_categories)
+    plot.figure.subplots_adjust(bottom=0.2)
+    plot.figure.savefig(imgdata, format="png", pad_inches=5)
     plot.clear()
     imgdata.seek(0)
     bot.send_photo(chat_id=update.message.chat_id, photo=imgdata)
